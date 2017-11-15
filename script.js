8735ec7ef61f9d6499f9fe42606be68db0befe2e
Robert Svensson <robertsvensson@users.noreply.github.com>
2017-11-15 22:35:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+  $( "#requestButton" ).click(function() {
+    $(".requestedImage").html(' <img src="//images.contentful.com/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg"> ');
+  });
+
+
+
+});
