4f1c4528d0dfcc012ded6296bba630efedb61e45
robertsvensson <robert.r.svensson@gmail.com>
2017-11-16 11:02:31 +0100

    Added support for setting image width and height

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
     var height = $("#imageHeight").val();
     var imageQuality = $("#imageQuality").val();
 
-    var requestURL = "https://images.contentful.com/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg?q=" + imageQuality +"";
+    var requestURL = "https://images.contentful.com/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg?w="+width+"&h="+height+"&fit=scale&q=" + imageQuality +"";
 
     $(".requestedImage").html(" <img src=" + requestURL + "> ");
 
