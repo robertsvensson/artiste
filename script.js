e3195851a59ac05703a63e0b78446ee50588d537
Robert Svensson <robert@artandhacks.se>
2017-11-16 08:52:19 +0100

    Added support for getting values from range elements

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ $( document ).ready(function() {
 
   $( "#requestButton" ).click(function() {
     $(".requestedImage").html(' <img src="//images.contentful.com/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg"> ');
+
+    var width = $("#imageWidth").val();
+    var height = $("#imageHeight").val();
+    var imageQuality = $("#imageQuality").val();
+
+    console.log(width);
+    console.log(height);
+    console.log(imageQuality);
+
   });
 
 
