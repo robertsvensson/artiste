60c46c0e0dfc4b051d6c76f502854705bcaf2a9f
Robert Svensson <robert@artandhacks.se>
2017-11-17 15:30:15 +0100

    added support for image padding and selecting the padding color

--- script.js
+++ script.js
@@ -6,7 +6,17 @@ $( document ).ready(function() {
     var height = $("#imageHeight").val();
     var imageQuality = $("#imageQuality").val();
 
-    var requestURL = "https://images.contentful.com/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg?w="+width+"&h="+height+"&fit=scale&q=" + imageQuality +"";
+    if ($("#paddingCheckbox").is(':checked')){
+      var fit = 'pad';
+      var paddingColorValue = $("#paddingColor").val().slice(1);
+
+    }
+    else{
+      var fit = 'scale';
+      var paddingColorValue = 'ffffff';
+    }
+
+    var requestURL = "https://images.contentful.com/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg?w="+width+"&h="+height+"&fit="+ fit +"&bg=rgb:"+paddingColorValue+"&q=" + imageQuality +"";
 
     $(".requestedImage").html(" <img src=" + requestURL + "> ");
     $("#jumbotronStatus").text("Request URL" + requestURL)
